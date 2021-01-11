1f078d4c12fb8637fc3a6cfc23528929acaa9847
Michael J Grayling <michael.grayling@newcastle.ac.uk>
2021-01-11 13:45:27 +0000

    Modified code to match assumptions of wider review

--- R/App.R
+++ R/App.R
@@ -16,7 +16,7 @@ source('PSAfunc.R',local=TRUE)
 
 ui <- dashboardPage(
   dashboardHeader(title = "PSAresp"),
-  
+
   dashboardSidebar(
     sidebarMenu(id="tab",
                 menuItem("Home", tabName = "home", icon = icon("home")),
@@ -24,7 +24,7 @@ ui <- dashboardPage(
                 menuItem("Source code", icon = icon("file-code"),href="https://github.com/martinamcm/PSAresp")
     )
   ),
-  
+
   dashboardBody(
     useShinyjs(),
     tabItems(
@@ -32,12 +32,12 @@ ui <- dashboardPage(
               fluidRow(
                 includeHTML("PSAHomePage.html")
               )),
-      
+
       tabItem(tabName="Analysis",
               fluidRow(
-                
+
                 box(title="Upload PSA Data",width=4,solidHeader = TRUE,status="primary",
-                    
+
                            # Input: Select a file ----
                            fileInput("file1", "Choose CSV File",
                                      multiple = FALSE,
@@ -46,113 +46,113 @@ ui <- dashboardPage(
                                                 ".csv")),
                            # Horizontal line ----
                            tags$hr(),
-                           
+
                            # Input: Checkbox if file has header ----
                            checkboxInput("header", "Header", TRUE),
-                           
+
                            # Input: Select separator ----
                            radioButtons("sep", "Separator",
                                         choices = c(Comma = ",",
                                                     Semicolon = ";",
                                                     Tab = "\t"),
                                         selected = ",")),
-                
-                
-                
-                box(title="Waterfall Plot",width=7,solidHeader = TRUE,status="primary",    
+
+
+
+                box(title="Waterfall Plot",width=7,solidHeader = TRUE,status="primary",
                     uiOutput("plotPSA")),
-                
+
                 box(title="Analysis", width=11,solidHeader = TRUE,status="primary",
-                    
+
                     column(width=3,
-                           numericInput("Thres", "Response threshold (%)",value=30,min=-100,max=100),
-                           numericInput("TruncValue","Truncation Value (%)",value=100,min=0,max=100000),
+                           numericInput("Thres", "Response threshold (%)",value=-30,min=-100,max=100),
                            actionButton("Run","Run Analysis")
                            ),
-                    
+
                     column(width=8,align="center",
                            uiOutput("ResultsTable"),
                            uiOutput("plot")
                            )
                     )
-                
+
                 )
               )
-              
-              
+
+
       )
     ))
 
 
 
 server <- function(input, output) {
-  
+
   InputData <- eventReactive(input$file1,{
     read.csv(input$file1$datapath)
   })
-  
+
   DataInf <- reactive(
     rawData <- InputData()
   )
-  
+
   output$ggwaterfall<-renderPlot({
-    
+
     req(DataInf())
-    
+
     PSAscore<-DataInf()[,1]
-    PSAdat <- data.frame(id=seq(1,length(PSAscore)) ,PSA=PSAscore[order(PSAscore)])
-    
+    PSAdat <- data.frame(id=seq(1,length(PSAscore)) ,PSA=PSAscore[rev(order(PSAscore))])
+
     ggplot(PSAdat, aes(x=id, y=PSA))+geom_hline(yintercept=input$Thres, linetype="dashed", color = "black")+
       xlab("Patient ID") + ylab("Change from baseline (%) in PSA score")+
       theme_bw() + theme(axis.text=element_text(size=12),
             axis.title.y = element_text(face="bold",angle=90,size=14), axis.title.x=element_text(size=14)) +
       coord_cartesian(ylim = c(-100,100))+
       geom_bar(stat="identity",width=0.7,position=position_dodge(width=0.4),fill="salmon1")
-    
+
   })
-  
+
   output$plotPSA <- renderUI(
     plotOutput("ggwaterfall",width="100%",height="350px")
   )
-  
+
   Analysis <- eventReactive(input$Run,{
-    
-    applyaugbin(DataInf()[,1],input$Thres,input$TruncValue)
-    
+
+    applyaugbin(DataInf()[,1], input$Thres)
+    applyaugbin(DataInf()[,1], input$Thres)
+
   })
-  
-  
+
+
   output$ResultsTable <- renderUI({
     formattableOutput("Results")%>% withSpinner(color="#0dc5c1")
   })
-  
+
   output$Results <- renderFormattable({
-    
+
     Method <- c("Augmented","Binary")
-    PointEst <- Analysis()[c(1,4)]
-    CIlower <- Analysis()[c(2,5)]
-    CIupper <- Analysis()[c(3,6)]
-    
+    PointEst <- Analysis()[c(4,1)]
+    CIlower <- Analysis()[c(5,2)]
+    CIupper <- Analysis()[c(6,3)]
+
     dataresultstable <- data.frame(Method,PointEst,CIlower,CIupper)
     formattable(dataresultstable,col.names=(c("Method","Point Estimate","95% CI lower","95% CI upper")),digits=3)
-    
+
   })
-  
+
   output$plot <- renderUI({
     plotOutput("p",width="75%",height="150px")
   })
-  
-  
+
+
   output$p<-renderPlot({
-    
+
     Method <- c("Augmented","Binary")
-    PointEst <- Analysis()[c(1,4)]
-    CIlower <- Analysis()[c(2,5)]
-    CIupper <- Analysis()[c(3,6)]
-    
+    PointEst <- Analysis()[c(4,1)]
+    CIlower <- Analysis()[c(5,2)]
+    CIupper <- Analysis()[c(6,3)]
+
     dataplot <- data.frame(Method,PointEst,CIlower,CIupper)
 
-    ggplot(dataplot,aes(x=as.factor(Method), y=PointEst, ymin=CIlower, ymax=CIupper, group=Method, 
+    ggplot(dataplot,aes(x=as.factor(Method), y=PointEst, ymin=CIlower, ymax=CIupper, group=Method,
                                color=Method))+
       geom_pointrange()+
       #geom_hline(yintercept = 0, linetype=3)+
@@ -164,14 +164,14 @@ server <- function(input, output) {
       theme(legend.title = element_text(size = 14,face="bold"),legend.text = element_text(size = 12),
             axis.text=element_text(size=12),
             axis.title=element_text(size=14))
-  
+
     })
-  
-  
+
+
 }
 
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
 
 
--- R/PSAfunc.R
+++ R/PSAfunc.R
@@ -1,69 +1,44 @@
 library('MASS')
 library('boot')
 library('Hmisc')
+library('car')
 
-meanprob=function(lm1,d)
-{
-  mean=lm1$coef[1]
-  
-  sigma=summary(lm1)$sigma
-  
-  prob=pnorm(d,mean,sigma)
-  
+applyaugbin <- function(data, dich) {
+  psadata         <- list(psa = -data, d = -dich)
+  truncationpoint <- 0.01
+  y               <- (100 - psadata$psa)/100
+  d               <- (100 - psadata$d)/100
+  # Set values of y below truncationpoint to truncationpoint
+  y               <- replace(y, which(y < truncationpoint), truncationpoint)
+  # Transform using best fitting boxcox model
+  lm_boxcox       <- lm(y1 ~ 1, data.frame(y1 = y), y=TRUE,qr=TRUE)
+  lambda          <- boxcox(lm_boxcox)$x[which.max(boxcox(lm_boxcox)$y)]
+  y               <- (y^lambda - 1)/lambda
+  d               <- (d^lambda - 1)/lambda
+  # augbin when no other covariates to adjust for
+  lm_augbin       <- lm(y ~ 1)
+  # Get binary analysis
+  S               <- ifelse(y < d, 1, 0)
+  results         <- c(binconf(x = sum(S), n = length(S)),
+                       deltamethod2(lm_augbin, d))
+  results[4:6]    <- 1 - results[4:6]
+  results[5:6]    <- results[6:5]
+  return(results)
+}
+
+meanprob    <- function(lm1, d) {
+  mean  <- lm1$coef[1]
+  sigma <- summary(lm1)$sigma
+  prob  <- 1 - pnorm(d,mean,sigma)
   return(logit(mean(prob)))
-  
 }
 
-deltamethod=function(lm1,d)
-{
-  prob=meanprob(lm1,d)
-  templm1=lm1
-  templm1$coef[1]=templm1$coef[1]+0.0001
-  derivative=(meanprob(templm1,d)-prob)/0.0001
-  
-  
-  var.meanprob=(summary(lm1)$coef[2]^2)*derivative^2
-  return(inv.logit(c(prob,prob-1.96*sqrt(var.meanprob),prob+1.96*sqrt(var.meanprob))))
+deltamethod <- function(lm1, d) {
+  prob            <- meanprob2(lm1, d)
+  templm1         <- lm1
+  templm1$coef[1] <- templm1$coef[1] + 0.0001
+  derivative      <- (meanprob2(templm1, d) - prob)/0.0001
+  var.meanprob    <- (summary(lm1)$coef[2]^2)*derivative^2
+  return(inv.logit(c(prob, prob - 1.96*sqrt(var.meanprob),
+                     prob + 1.96*sqrt(var.meanprob))))
 }
-
-
-
-
-applyaugbin=function(data,dich,truncationpoint)
-{
-  #change scale to relative change from baseline (2 being 100% increase, 1 being 0% decrease, 0 representing 100% decrease)  
-  
-  y=(100-as.numeric(data))/100  
-  d=(100-dich)/100
-  trunc=(100-truncationpoint)/100
-  
-  #set values of y below truncationpoint to truncationpoint
-  
-  y=replace(y,which(y<trunc),trunc)
-  
-  #Get binary analysis:
-  S=ifelse(y<d,1,0)
-  
-  bin<-binconf(x = sum(S),n = length(S))
-  
-  ##aug
-  y=y+abs(min(y))+0.00001
-  d=d+abs(min(y))+0.00001
-  
-  #transform using best fitting boxcox model:
-  PSAdat=data.frame(y=y)
-  lm.boxcox=lm(y~1,data=PSAdat,y=TRUE,qr=TRUE)
-  lambda=boxcox(lm.boxcox)$x[which.max(boxcox(lm.boxcox)$y)]
-  
-  y=(y^lambda-1)/lambda
-  d=(d^lambda-1)/lambda
-  
-  #augbin when no other covariates to adjust for:
-  lm.augbin=lm(y~1)
-  
-  
-  aug<-deltamethod(lm.augbin,d)
-  
-  return(c(aug,bin))
-}
-
